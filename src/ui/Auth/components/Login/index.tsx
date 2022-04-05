37af6ec330abad04b76b5aa8286ed805cbd95133
Samir Mirzaliyev <themirzaliyev@Samirs-MacBook-Air.local>
2022-04-05 12:20:29 +0400

    updated jwt settings

--- src/ui/Auth/components/Login/index.tsx
+++ src/ui/Auth/components/Login/index.tsx
@@ -37,7 +37,7 @@ const Login = () => {
           {
             headers: {
               'Content-Type': 'application/json',
-              'credintials': 'include',
+              credintials: 'include',
             },
           },
         );
